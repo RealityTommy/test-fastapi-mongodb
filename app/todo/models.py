509386a34ed2d4a79dd2a18b1816c79ddd425e26
Tommy Truong <ttruong@emergejobs.com>
2024-08-21 09:23:23 -0400

    Updated models with example schema

--- app/todo/models.py
+++ app/todo/models.py
@@ -1,7 +1,19 @@
 from pydantic import BaseModel
 
+# Define the TodoModel
 class TodoModel(BaseModel):
-    name: str = "Name"
-    description: str = "Description"
-    completed: bool = False
+    name: str
+    description: str
+    completed: bool
     uid: str
+
+    # Add an example to the JSON schema
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "name": "Name",
+                "description": "Description",
+                "completed": False,
+                "uid": ""
+            }
+        }
--- app/user/models.py
+++ app/user/models.py
@@ -3,12 +3,24 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+# Load environment variables
 dotenv_path = join(dirname(__file__), '../../.env')
 load_dotenv(dotenv_path)
 first_name = os.environ.get("TEST_FIRST_NAME")
 last_name = os.environ.get("TEST_LAST_NAME")
 
+# Define the UserModel
 class UserModel(BaseModel):
-    first_name: str = first_name
-    last_name: str = last_name
-    firebase_uid: str = None
+    first_name: str
+    last_name: str
+    firebase_uid: str
+
+    # Add an example to the JSON schema
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "first_name": first_name,
+                "last_name": last_name,
+                "firebase_uid": ""
+            }
+        }
